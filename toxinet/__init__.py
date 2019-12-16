3948ad8aec512892abc5743954d57b697f0fcbf0
Arpan Mukherjee <30600328+arpanisi@users.noreply.github.com>
2019-12-16 15:24:28 -0500

    Update __init__.py

--- toxinet/__init__.py
+++ toxinet/__init__.py
@@ -1,4 +1,5 @@
 from classifier import Classifier
 from load_data import load_training_data, plot_corr_diag, create_new_predicted_data
+from diagnostics import Activation
 
-__all__ = [Classifier, load_training_data, plot_corr_diag, create_new_predicted_data]
+__all__ = [Classifier, load_training_data, plot_corr_diag, create_new_predicted_data, Activation]
